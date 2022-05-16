a3d5bfb33a9b9f5176f2e88a3e7b23a8d56ed25e
Diego Fernando Alayon <dfar.41@gmail.com>
2022-05-16 00:15:36 -0500

    agregando 0 a vista de DisplayTimer

--- src/components/DisplayTimer/index.jsx
+++ src/components/DisplayTimer/index.jsx
@@ -1,7 +1,11 @@
 export default function DisplayTimer ({ minutes, seconds }) {
+  console.log(typeof minutes)
+  const addZero = (value) => {
+    return value < 10 ? '0' + value : value
+  }
   return (
     <>
-      <h2>{minutes}:{seconds}</h2>
+      <h2>{addZero(minutes)}:{addZero(seconds)}</h2>
     </>
   )
 }
