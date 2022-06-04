3270a988b33464e3994a64ba69d2a06c48f0e92a
Diego Fernando Alayon <dfar.41@gmail.com>
2022-06-03 19:53:26 -0500

    agregando funcionalidad al componente InputTimerForm

--- src/components/InputTimerForm/index.jsx
+++ src/components/InputTimerForm/index.jsx
@@ -1,17 +1,22 @@
 // components
 import Up from '../Icons/Up'
 import Down from '../Icons/Down'
-export default function InputTimerForm ({ title, value, handleChange } = {}) {
+// styles
+import './inputTimerForm.css'
+export default function InputTimerForm ({ title, value, update, up, down } = {}) {
+  const handleChange = (event) => {
+    const { value } = event.target
+    update(value)
+  }
   return (
     <>
-
-      <div>
+      <div className='input-container'>
         <span>{title}</span>
         <div>
           <input type='text' onChange={handleChange} value={value} />
           <div>
-            <Up />
-            <Down />
+            <Up onClick={up} />
+            <Down onClick={down} />
           </div>
         </div>
       </div>
