bb0a33b15d7e65590e0c583a8d83d944146d9b52
Diego Fernando Alayon <dfar.41@gmail.com>
2023-02-01 00:31:50 -0500

    modificando main para usar react-router-dom v6

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+
 import './index.css'
+import Timer from './pages/Timer'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Navigate to='/casa'/>,
+    children: [
+      {
+        path: '/timer',
+        element: <Timer />
+      },
+      {
+        path: '/chronometer/',
+        element: <h1>casita</h1>
+      },
+      {
+        path: '/countdown',
+        element: <h1>CountDown</h1>
+      },
+      {
+        path: '/timero',
+        element: <h1>Timer</h1>
+      }
+    ]
+  },
+  {
+    path: '/casa',
+    element: <h1>hola mundo</h1>
+  },
+  {
+    path: '/*',
+    element: <h1>esta es la ruta default</h1>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
