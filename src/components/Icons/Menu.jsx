6babf825e62967db1da3dd7863ff703d53cfcb06
Diego Fernando Alayon <dfar.41@gmail.com>
2023-01-31 19:36:11 -0500

    creando componente Icon/Menu

---
+++ src/components/Icons/Menu.jsx
@@ -0,0 +1,22 @@
+const Menu = (props) => {
+  return(
+
+    <svg
+      height={21}
+      width={12}
+      viewBox="0 0 21 21"
+      {...props}
+    >
+      <g
+        fill='none'
+        fillRule='evenodd'
+        stroke={props.stroke || 'currentColor'}
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      >
+        <path d='M4.5 6.5h12M4.498 10.5h11.997M4.5 14.5h11.995' />
+      </g>
+    </svg>
+  )
+}
+export default Menu
