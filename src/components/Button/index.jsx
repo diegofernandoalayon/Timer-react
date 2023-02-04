d8e7c920dc8a5f378f3c364302d68d43bc6b3ae9
Diego Fernando Alayon <dfar.41@gmail.com>
2023-02-04 17:00:16 -0500

    modificando Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,12 +1,13 @@
 import './button.css'
-export default function Button ({ children, onClick, isPaused }) {
+export default function Button ({ children, onClick, isPaused, submit }) {
   let className = 'btn'
   if (isPaused) {
     className += ' btn-pause'
   }
+  const typeButton = submit ? 'submit' : 'button'
   return (
     <>
-      <button className={className} onClick={onClick}>{children}</button>
+      <button type={typeButton} className={className} onClick={onClick}>{children}</button>
     </>
   )
 }
