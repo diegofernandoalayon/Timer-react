3b4a156e3b31731d3df0ba1bd518f4ba6ab1d658
Diego Fernando Alayon <dfar.41@gmail.com>
2023-02-04 17:06:06 -0500

    modificaciones App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+// import { useEffect } from 'react'
+// import { useNavigate } from 'react-router-dom'
 import './App.css'
-import Timer from './pages/Timer'
+import { Outlet } from 'react-router'
+import Navbar from './components/Navbar'
 
 function App () {
+  // const navigate = useNavigate()
+  // useEffect(() => {
+
+  //   navigate('/casa')
+  // },[])
   return (
-    <article className='App'>
-
-      {/* <DisplayTimer /> */}
-      <Timer />
-
+    <article className="App">
+      <Navbar />
+      <Outlet />
     </article>
   )
 }
