03c3a1d500629bbe4a73122ac2613a184df104f0
Diego Fernando Alayon <dfar.41@gmail.com>
2023-02-04 21:06:03 -0500

    detectando pestaña activa

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,14 +1,25 @@
 import { Link } from 'react-router-dom'
 import './navbar.css'
+import { useState } from 'react'
+// import { useRef } from 'react'
 const ITEMS_NAV = ['chronometer', 'timer', 'countdown']
 const Navbar = () => {
+  const [activeTab, setActiveTab] = useState(null)
+  const handle = (item) => {
+    setActiveTab(item)
+  }
+  console.log('casas casac acsacasa')
   return (
     <nav className="nav-bar">
       <ul>
         {
           ITEMS_NAV.map(item => (
-          <li key={item}>
-            <Link to={item}>{item}</Link>
+          <li key={item} onClick={() => handle(item)} className={`${activeTab === item ? 'active' : ''}`}>
+            <Link
+            to={item}
+            >
+              {item}
+            </Link>
           </li>
           ))
         }
