7d5887907a61d5177f786ed9cc2f11cf9183cad6
Diego Fernando Alayon <dfar.41@gmail.com>
2023-03-07 23:19:59 -0500

    creando componente FullScreenButton

---
+++ src/components/FullScreenButton/index.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import FullScreen from '../Icons/FullScreen'
+import Minimise from '../Icons/Minimise'
+import './fullscreenbutton.css'
+const FullScreenButton = ({ element }) => {
+  const [isFullScreen, setIsFullScreen] = useState(false)
+  const handleScreen = () => {
+    if (window.innerHeight === window.screen.height) {
+      document.exitFullscreen()
+      setIsFullScreen(false)
+    } else {
+      element.current.requestFullscreen()
+      setIsFullScreen(true)
+    }
+  }
+  return (
+    <button
+      className="btn-fullscreen"
+      onClick={handleScreen}
+      // style={{ color: 'inherit' }}
+    >
+      {
+        isFullScreen
+          ? <Minimise height={46} width={46}/>
+          : <FullScreen height={46} width={46}/>
+      }
+    </button>
+  )
+}
+export default FullScreenButton
