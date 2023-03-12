af31ce15c4320be32e29dc04e2538bae220fab65
Diego Fernando Alayon <dfar.41@gmail.com>
2023-03-12 11:11:07 -0500

    agregando contador de horas

--- src/components/DisplayTimer/index.jsx
+++ src/components/DisplayTimer/index.jsx
@@ -1,10 +1,14 @@
-export default function DisplayTimer ({ minutes, seconds }) {
+export default function DisplayTimer ({ hours = 0, minutes, seconds } = {}) {
   const addZero = (value) => {
     return value < 10 ? '0' + value : value
   }
   return (
     <>
-      <h2 style={{ color: 'var(--color-primary)' }}>{addZero(minutes)}:{addZero(seconds)}</h2>
+      <h2
+        style={{ color: 'var(--color-primary)' }}
+      >
+        {hours !== 0 ? `${addZero(hours)}:` : null}{addZero(minutes)}:{addZero(seconds)}
+      </h2>
     </>
   )
 }
