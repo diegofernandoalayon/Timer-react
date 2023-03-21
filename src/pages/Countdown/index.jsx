134a35f9fd1f5bd4c7ccea71799259831a83c778
Diego Fernando Alayon <dfar.41@gmail.com>
2023-03-20 21:07:21 -0500

    creacion de page Countdown

---
+++ src/pages/Countdown/index.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect, useRef } from 'react'
+// import DisplayTimer from '../../components/DisplayTimer'
+import Button from '../../components/Button'
+import DisplayCountdown from '../../components/DisplayCountdown'
+function Countdown () {
+  const [timeState, setTimeState] = useState(0)
+  const [timeRef, setTimeRef] = useState(0)
+  const interval = useRef(0)
+  const startTimer = () => {
+    setTimeState(+timeRef - +new Date().getTime())
+    interval.current = setInterval(() => {
+      setTimeState(+timeRef - +new Date().getTime())
+    }, 1000)
+  }
+  useEffect(() => {
+    const userSettings = JSON.parse(window.localStorage.getItem('user-settings'))
+    const { count } = userSettings
+    if (count) setTimeRef(count)
+  }, [])
+
+  useEffect(() => {
+    startTimer()
+    return () => clearInterval(interval.current)
+  }, [timeRef])
+  const handleClick = () => {
+    const count = new Date('Jun 14, 2023 23:59:00').getTime()
+    const userSettings = window.localStorage.getItem('user-settings')
+    console.log('userSettings: ', userSettings)
+    const userSettingsParsed = JSON.parse(userSettings)
+    window.localStorage.setItem('user-settings', JSON.stringify({ ...userSettingsParsed, count }))
+  }
+  const days = Math.floor(timeState / (1000 * 60 * 60 * 24))
+  const hours = Math.floor((timeState % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  const minutes = Math.floor((timeState % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds = Math.floor((timeState % (1000 * 60)) / 1000)
+
+  console.log(hours)
+  return (
+    <>
+      <section>
+          <DisplayCountdown
+            days={days}
+            hours ={hours}
+            minutes={minutes}
+            seconds={seconds}
+          />
+
+        <Button onClick={handleClick}>Establecer</Button>
+      </section>
+    </>
+  )
+}
+
+export default Countdown
