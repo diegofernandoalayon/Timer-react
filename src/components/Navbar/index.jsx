dbbc4aa1a02d10a9453decbe0e9ce085fc164123
Diego Fernando Alayon <dfar.41@gmail.com>
2023-05-28 13:53:41 -0500

    modificando Navbar agregando botones de dark-mode y muted

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,17 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import Sun from '../Icons/Sun'
 import './navbar.css'
+import Moon from '../Icons/Moon'
+import { SettingsContext } from '../../context/SettingsContext'
+import VolumeMuted from '../Icons/VolumeMuted'
+import VolumeHigh from '../Icons/VolumeHigh'
 
 const ITEMS_NAV = ['chronometer', 'timer', 'countdown']
 const Navbar = ({ handleOptionSelected }) => {
   const [activeTab, setActiveTab] = useState(null)
+  const { isDark, changeIsDark, isMuted, changeIsMuted } = useContext(SettingsContext)
+
   const { pathname } = useLocation()
   const path = pathname.slice(1)
 
@@ -18,19 +25,42 @@ const Navbar = ({ handleOptionSelected }) => {
 
   return (
     <nav className="nav-bar">
-      <ul>
-        {
-          ITEMS_NAV.map(item => (
-          <li key={item} onClick={() => handle(item)} className={`${activeTab === item ? 'active' : ''}`}>
-            <Link
-            to={item}
-            >
-              {item}
-            </Link>
+      <div>
+        <ul className='settings'>
+          <li>
+            <button className='btn-dark-mode' onClick={() => changeIsDark()}>
+              {
+                isDark
+                  ? <Sun />
+                  : <Moon />
+              }
+            </button>
           </li>
-          ))
-        }
-      </ul>
+          <li>
+            <button className='btn-dark-mode' onClick={changeIsMuted}>
+              {
+                isMuted
+                  ? <VolumeMuted />
+                  : <VolumeHigh />
+              }
+            </button>
+          </li>
+        </ul>
+        <ul className='options'>
+          {
+            ITEMS_NAV.map(item => (
+            <li key={item} onClick={() => handle(item)} className={`${activeTab === item ? 'active' : ''}`}>
+              <Link
+              to={item}
+              >
+                {item}
+              </Link>
+            </li>
+            ))
+          }
+        </ul>
+      </div>
+
     </nav>
   )
 }
