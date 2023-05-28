99f8403597a2ff7487232daca5df42a94a860939
Diego Fernando Alayon <dfar.41@gmail.com>
2023-05-28 13:52:50 -0500

    modificando SettingsContext agregando estado para isMuted, creando logica de guardado de settings en LocalStorage, y color automatico la primera vez dependiendo de las preferencias del usuario

--- src/context/SettingsContext.jsx
+++ src/context/SettingsContext.jsx
@@ -1,13 +1,40 @@
-// TODO: crear el context para tener el theme global, con el estado controlar los icons,
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 export const SettingsContext = createContext()
 
 export const SettingsContextProvider = ({ children }) => {
   const [isDark, setIsDark] = useState(true)
-  const changeIsDark = () => setIsDark((a) => !a)
+  const [isMuted, setIsMuted] = useState(false)
+  useEffect(() => {
+    const userSettings = window.localStorage.getItem('user-settings')
+    if (!userSettings) return
+    const { isDark, isMuted } = JSON.parse(userSettings)
+    if (isMuted !== undefined) setIsMuted(isMuted)
+    if (isDark !== undefined) {
+      setIsDark(isDark)
+    } else {
+      const themeUser = window.matchMedia('(prefers-color-scheme: dark)').matches
+      setIsDark(themeUser)
+    }
+  }, [])
+  const changeIsDark = () => {
+    setIsDark((a) => {
+      const userSettings = window.localStorage.getItem('user-settings')
+      const userSettingsParsed = JSON.parse(userSettings)
+      window.localStorage.setItem('user-settings', JSON.stringify({ ...userSettingsParsed, isDark: !a }))
+      return !a
+    })
+  }
+  const changeIsMuted = () => {
+    setIsMuted((a) => {
+      const userSettings = window.localStorage.getItem('user-settings')
+      const userSettingsParsed = JSON.parse(userSettings)
+      window.localStorage.setItem('user-settings', JSON.stringify({ ...userSettingsParsed, isMuted: !a }))
+      return !a
+    })
+  }
   return (
-    <SettingsContext.Provider value={{ isDark, changeIsDark }}>
+    <SettingsContext.Provider value={{ isDark, changeIsDark, isMuted, changeIsMuted }}>
       {children}
     </SettingsContext.Provider>
   )
