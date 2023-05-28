5d142d85bcdb57e03a64c1f731701706c8385990
Diego Fernando Alayon <dfar.41@gmail.com>
2023-05-28 13:43:25 -0500

    usando isDark para determinar el theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useRef } from 'react'
+import { useContext, useRef } from 'react'
 import './App.css'
 import { Outlet } from 'react-router'
 import Header from './components/Header'
 import FullScreenButton from './components/FullScreenButton'
+import { SettingsContext } from './context/SettingsContext'
 
 function App () {
   const reference = useRef(null)
+  const { isDark } = useContext(SettingsContext)
 
   return (
-    <article className="App">
+    <article className={`App ${isDark ? '' : 'light-mode'}`}>
       <Header />
       <section ref={reference} className='container-screen'>
-
         <div className = 'container-btn-full'>
           <FullScreenButton element={reference}/>
         </div>
