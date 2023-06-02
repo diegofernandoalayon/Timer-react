1f2a733c7115e651f5102893c5c47acfa33196a0
Diego Fernando Alayon <dfar.41@gmail.com>
2023-06-02 14:55:09 -0500

    creando utils para calcular tiempo

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,26 @@
 export const addZero = (value) => {
   return value < 10 ? '0' + value : value
 }
+
+export const calculateTimeMilli = (timeStamp) => {
+  const days = Math.floor(timeStamp / (1000 * 60 * 60 * 24))
+  const hours = Math.floor((timeStamp % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  const minutes = Math.floor((timeStamp % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds = Math.floor((timeStamp % (1000 * 60)) / 1000)
+  return {
+    days,
+    hours,
+    minutes,
+    seconds
+  }
+}
+export const calculateTimeSeconds = (timeStamp) => {
+  const hours = Math.floor((timeStamp % (60 * 60 * 60)) / (60 * 60))
+  const minutes = Math.floor((timeStamp % (60 * 60)) / (60))
+  const seconds = Math.floor((timeStamp % 60))
+  return {
+    hours,
+    minutes,
+    seconds
+  }
+}
